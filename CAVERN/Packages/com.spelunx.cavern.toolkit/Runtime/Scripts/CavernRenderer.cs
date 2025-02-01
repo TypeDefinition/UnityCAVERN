649dac2caf1f0ce399a0260e7e1a1742aaf8450a
TerryLim <terrylim@andrew.cmu.edu>
2025-01-31 20:03:19 -0500

    Add support for (non-head tracked) stereoscopic rendering.

--- CAVERN/Packages/com.spelunx.cavern.toolkit/Runtime/Scripts/CavernRenderer.cs
+++ CAVERN/Packages/com.spelunx.cavern.toolkit/Runtime/Scripts/CavernRenderer.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using System.Collections.Generic;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Rendering.RenderGraphModule.Util;
-using UnityEngine.Rendering.RenderGraphModule;
 
 namespace Spelunx {
-    [RequireComponent(typeof(Camera))]
     public class CavernRenderer : MonoBehaviour {
-        class ProjectionPass : ScriptableRenderPass {
+        /*class ProjectionPass : ScriptableRenderPass {
             private Material material;
 
             public ProjectionPass(Material material) {
@@ -31,36 +27,49 @@ namespace Spelunx {
                 RenderGraphUtils.BlitMaterialParameters blitParams = new RenderGraphUtils.BlitMaterialParameters(emptySource, target, material, 0);
                 renderGraph.AddBlitPass(blitParams, "Cavern Projection Pass");
             }
-        }
+        }*/
 
-        public enum StereoMode { Off, On, }
+        public enum StereoscopicMode { Mono, Stereo, }
+        
         public enum EyeResolution {
             Low = 1024,
             Mid = 2048,
             High = 4096,
+            VeryHigh = 8192,
+        }
+
+        private enum CubemapIndex {
+            // Monoscopic
+            Mono = 0,
+
+            // Stereoscopic
+            Left = 0,
+            Right,
+            Front,
+            Back,
+
+            Num,
         }
 
         [Header("Camera Settings")]
-        [SerializeField] private StereoMode stereoMode = StereoMode.On;
-        [SerializeField] private EyeResolution eyeResolution = EyeResolution.High;
-        [SerializeField, Range(0.0f, 1.0f)] private float interpupillaryDistance = 0.064f; // IPD in metres.
+        [SerializeField] private StereoscopicMode stereoMode = StereoscopicMode.Mono;
+        [SerializeField] private EyeResolution eyeResolution = EyeResolution.Mid;
+        [SerializeField, Range(0.0f, 1.0f)] private float interpupillaryDistance = 0.1f; // IPD in metres.
         [SerializeField, Min(0.1f)] private float cavernHeight = 2.0f; // Cavern physical screen height in metres.
         [SerializeField, Min(0.1f)] private float cavernRadius = 3.0f; // Cavern physical screen radius in metres.
         [SerializeField, Min(0.1f)] private float cavernAngle = 270.0f; // Cavern physical screen angle in degrees.
+        [SerializeField, Range(-1.0f, 1.0f)] private float cavernElevation = 0.0f; // Cavern physical screen elevation off the floor in metres.
 
         [Header("References")]
         [SerializeField] private Shader shader;
-        [SerializeField] private Camera leftEye; // Have to do this for now because stereoSeperation is broken in Unity 6. Cannot use Camera.stereoSeperation.
-        [SerializeField] private Camera rightEye;
+        [SerializeField] private Camera captureCamera;
 
-        private RenderTexture cubemapMonoEye; // Mono
-        private RenderTexture cubemapLeftEye; // Left Eye
-        private RenderTexture cubemapRightEye; // Right Eye
+        private RenderTexture[] cubemaps;
         private Material material;
-        private ProjectionPass projectionPass; // This does not work.
+        // private ProjectionPass projectionPass; // This does not work.
 
         public float GetIPD() { return interpupillaryDistance; }
-        public StereoMode GetStereoMode() { return stereoMode; }
+        public StereoscopicMode GetStereoscopicMode() { return stereoMode; }
 
         private void OnEnable() {
             RenderPipelineManager.beginContextRendering += OnBeginContextRendering;
@@ -78,27 +87,24 @@ namespace Spelunx {
 
         private void Awake() {
             // Initialise render textures.
-            cubemapMonoEye = new RenderTexture((int)eyeResolution, (int)eyeResolution, 32, RenderTextureFormat.ARGB32);
-            cubemapMonoEye.dimension = TextureDimension.Cube;
-            cubemapMonoEye.wrapMode = TextureWrapMode.Clamp;
-
-            cubemapLeftEye = new RenderTexture((int)eyeResolution, (int)eyeResolution, 32, RenderTextureFormat.ARGB32);
-            cubemapLeftEye.dimension = TextureDimension.Cube;
-            cubemapLeftEye.wrapMode = TextureWrapMode.Clamp;
-
-            cubemapRightEye = new RenderTexture((int)eyeResolution, (int)eyeResolution, 32, RenderTextureFormat.ARGB32);
-            cubemapRightEye.dimension = TextureDimension.Cube;
-            cubemapRightEye.wrapMode = TextureWrapMode.Clamp;
-
+            cubemaps = new RenderTexture[(int)CubemapIndex.Num];
+            for (int i = 0; i < (int)CubemapIndex.Num; ++i) {
+                cubemaps[i] = new RenderTexture((int)eyeResolution, (int)eyeResolution, 32, RenderTextureFormat.ARGB32);
+                cubemaps[i].dimension = TextureDimension.Cube;
+                cubemaps[i].wrapMode = TextureWrapMode.Clamp;
+            }
+            
             // Initialise material.
             material = new Material(shader);
-            material.SetTexture("_CubemapMonoEye", cubemapMonoEye);
-            material.SetTexture("_CubemapLeftEye", cubemapLeftEye);
-            material.SetTexture("_CubemapRightEye", cubemapRightEye);
+            material.SetTexture("_CubemapMono", cubemaps[(int)CubemapIndex.Mono]);
+            material.SetTexture("_CubemapLeft", cubemaps[(int)CubemapIndex.Left]);
+            material.SetTexture("_CubemapRight", cubemaps[(int)CubemapIndex.Right]);
+            material.SetTexture("_CubemapFront", cubemaps[(int)CubemapIndex.Front]);
+            material.SetTexture("_CubemapBack", cubemaps[(int)CubemapIndex.Back]);
 
             // Initialise projection pass.
-            projectionPass = new ProjectionPass(material);
-            projectionPass.renderPassEvent = RenderPassEvent.AfterRendering;
+            // projectionPass = new ProjectionPass(material);
+            // projectionPass.renderPassEvent = RenderPassEvent.AfterRendering;
         }
 
         private void Start() {
@@ -109,38 +115,44 @@ namespace Spelunx {
         }
 
         private void RenderEyes() {
-            int faceMask = 0;
-            faceMask |= 1 << (int)CubemapFace.PositiveX;
-            faceMask |= 1 << (int)CubemapFace.NegativeX;
-            faceMask |= 1 << (int)CubemapFace.PositiveY;
-            faceMask |= 1 << (int)CubemapFace.NegativeY;
-            faceMask |= 1 << (int)CubemapFace.PositiveZ;
-            faceMask |= 1 << (int)CubemapFace.NegativeZ;
+            const int leftMask = 1 << (int)CubemapFace.PositiveX;
+            const int rightMask = 1 << (int)CubemapFace.NegativeX;
+            const int topMask = 1 << (int)CubemapFace.PositiveY;
+            const int bottomMask = 1 << (int)CubemapFace.NegativeY;
+            const int frontMask = 1 << (int)CubemapFace.PositiveZ;
+            const int backMask = 1 << (int)CubemapFace.NegativeZ;
 
-            Camera camera = GetComponent<Camera>();
             switch (stereoMode) {
-                case StereoMode.Off:
-                    camera.RenderToCubemap(cubemapMonoEye, faceMask, Camera.MonoOrStereoscopicEye.Mono);
-                    break;
-                case StereoMode.On:
-                    camera.stereoSeparation = interpupillaryDistance;
-                    // camera.RenderToCubemap(cubemapLeftEye, faceMask, Camera.MonoOrStereoscopicEye.Left);
-                    // camera.RenderToCubemap(cubemapRightEye, faceMask, Camera.MonoOrStereoscopicEye.Right);
+                case StereoscopicMode.Mono:
+                    captureCamera.stereoSeparation = 0.0f;
+                    captureCamera.transform.localPosition = Vector3.zero;
+                    captureCamera.transform.localRotation = Quaternion.identity;
 
-                    leftEye.transform.localPosition = new Vector3(-interpupillaryDistance * 0.5f, 0.0f, 0.0f);
-                    leftEye.transform.localRotation = Quaternion.identity;
-                    rightEye.transform.localPosition = new Vector3(interpupillaryDistance * 0.5f, 0.0f, 0.0f);
-                    rightEye.transform.localRotation *= Quaternion.identity;
-                    leftEye.RenderToCubemap(cubemapLeftEye, faceMask, Camera.MonoOrStereoscopicEye.Mono);
-                    rightEye.RenderToCubemap(cubemapRightEye, faceMask, Camera.MonoOrStereoscopicEye.Mono);
+                    // Use Camera.MonoOrStereoscopicEye.Left or Camera.MonoOrStereoscopicEye.Right to ensure that the cubemap follows the camera's rotation.
+                    // Camera.MonoOrStereoscopicEye.Mono renders the cubemap to be aligned to the world's axes instead.
+                    captureCamera.RenderToCubemap(cubemaps[(int)CubemapIndex.Mono], frontMask | leftMask | rightMask, Camera.MonoOrStereoscopicEye.Left);
+                    break;
+                case StereoscopicMode.Stereo:
+                    // captureCamera.stereoSeparation = interpupillaryDistance;
+                    captureCamera.stereoSeparation = 0.0f;
+                    captureCamera.transform.localRotation = Quaternion.identity;
+                    captureCamera.transform.localPosition = new Vector3(-interpupillaryDistance * 0.5f, 0.0f, 0.0f);
+                    captureCamera.RenderToCubemap(cubemaps[(int)CubemapIndex.Left], frontMask, Camera.MonoOrStereoscopicEye.Left);
+                    captureCamera.transform.localPosition = new Vector3(interpupillaryDistance * 0.5f, 0.0f, 0.0f);
+                    captureCamera.RenderToCubemap(cubemaps[(int)CubemapIndex.Right], frontMask, Camera.MonoOrStereoscopicEye.Right);
+                    captureCamera.transform.localPosition = new Vector3(0.0f, 0.0f, interpupillaryDistance * 0.5f);
+                    captureCamera.RenderToCubemap(cubemaps[(int)CubemapIndex.Front], leftMask | rightMask, Camera.MonoOrStereoscopicEye.Left);
+                    captureCamera.transform.localPosition = new Vector3(0.0f, 0.0f, -interpupillaryDistance * 0.5f);
+                    captureCamera.RenderToCubemap(cubemaps[(int)CubemapIndex.Back], leftMask | rightMask, Camera.MonoOrStereoscopicEye.Right);
                     break;
             }
 
-            material.SetInteger("_EnableStereo", stereoMode == StereoMode.On ? 1 : 0);
+            material.SetInteger("_EnableStereo", stereoMode == StereoscopicMode.Stereo ? 1 : 0);
             material.SetFloat("_CavernHeight", cavernHeight);
             material.SetFloat("_CavernRadius", cavernRadius);
             material.SetFloat("_CavernAngle", cavernAngle);
-            material.SetMatrix("_CameraRotation", Matrix4x4.Rotate(transform.rotation));
+            material.SetFloat("_CavernElevation", cavernElevation);
+            material.SetMatrix("_CameraRotation", Matrix4x4.identity);
         }
 
         private void OnBeginContextRendering(ScriptableRenderContext context, List<Camera> cameras) {
@@ -150,14 +162,14 @@ namespace Spelunx {
         }
 
         private void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera) {
-            if (camera == GetComponent<Camera>()) {
+            if (camera == captureCamera) {
                 // UniversalRenderPipelineAsset urpAsset = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
                 // urpAsset.scriptableRenderer.EnqueuePass(projectionPass);
             }
         }
 
         private void OnEndCameraRendering(ScriptableRenderContext context, Camera camera) {
-            if (camera == GetComponent<Camera>()) {
+            if (camera == captureCamera) {
                 Graphics.Blit(null, material);
             }
         }
